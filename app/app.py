a288afffdec0b501b66e7c29910af6e406d0a7d5
Bartek <barteksadlej@gmail.com>
2022-01-10 12:45:19 +0100

    added db connection in app

--- app/app.py
+++ app/app.py
@@ -1,8 +1,59 @@
 from flask import Flask
 from flask import render_template
 
+import os
+import sys
+import cx_Oracle
+
+
+from user_secrets import PYTHON_USERNAME, PYTHON_PASSWORD
+
+os.environ['PYTHON_USERNAME'] = PYTHON_USERNAME
+os.environ['PYTHON_PASSWORD'] = PYTHON_PASSWORD
+os.environ['PYTHON_CONNECTSTRING'] = "//labora.mimuw.edu.pl/LABS"
+
+
+if sys.platform.startswith("darwin"):
+    cx_Oracle.init_oracle_client(lib_dir=os.environ.get("HOME")+"/instantclient_19_3")
+elif sys.platform.startswith("win32"):
+    cx_Oracle.init_oracle_client(lib_dir=r"c:\oracle\instantclient_19_8")
+
+def init_session(connection, requestedTag_ignored):
+    cursor = connection.cursor()
+    cursor.execute("""
+        ALTER SESSION SET
+          TIME_ZONE = 'UTC'
+          NLS_DATE_FORMAT = 'YYYY-MM-DD HH24:MI'""")
+
+def start_pool():
+
+    pool_min = 4
+    pool_max = 4
+    pool_inc = 0
+    pool_gmd = cx_Oracle.SPOOL_ATTRVAL_WAIT
+
+    print("Connecting to", os.environ.get("PYTHON_CONNECTSTRING"))
+
+    pool = cx_Oracle.SessionPool(user=os.environ.get("PYTHON_USERNAME"),
+                                 password=os.environ.get("PYTHON_PASSWORD"),
+                                 dsn=os.environ.get("PYTHON_CONNECTSTRING"),
+                                 min=pool_min,
+                                 max=pool_max,
+                                 increment=pool_inc,
+                                 threaded=True,
+                                 getmode=pool_gmd,
+                                 sessionCallback=init_session)
+
+    return pool
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("base.html")
+
+
+if __name__ == '__main__':
+
+    pool = start_pool()
+    app.run(port=int(os.environ.get('PORT', '8080')))
